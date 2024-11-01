a0e2eae577e98d2a175289606fc9dcb0f210ffdd
mubai <chaokun.yck@antgroup.com>
2024-11-01 14:35:12 +0800

    register class only when class id is specified

--- deployment/src/main/java/io/quarkus/fury/deployment/FurySerializerBuildItem.java
+++ deployment/src/main/java/io/quarkus/fury/deployment/FurySerializerBuildItem.java
@@ -1,8 +1,10 @@
 package io.quarkus.fury.deployment;
 
 import io.quarkus.builder.item.SimpleBuildItem;
+import io.quarkus.fury.runtime.FurySerialization;
 import org.apache.fury.Fury;
 import org.apache.fury.serializer.Serializer;
+import org.apache.fury.util.Preconditions;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.JandexReflection;
 
@@ -13,7 +15,15 @@ public class FurySerializerBuildItem extends SimpleBuildItem {
 
   public FurySerializerBuildItem(ClassInfo classInfo) {
     Class<?> clazz = JandexReflection.loadClass(classInfo);
-    fury.register(clazz, true);
+    FurySerialization annotation = clazz.getDeclaredAnnotation(FurySerialization.class);
+    int classId = annotation.classId();
+    if (classId > 0 && classId < Short.MAX_VALUE) {
+      Class<?> registeredClass = fury.getClassResolver().getRegisteredClass((short) classId);
+      Preconditions.checkArgument(registeredClass == null,
+        "ClassId %s has been registered for class %s", classId, registeredClass);
+    } else {
+      fury.register(clazz, (short) classId, true);
+    }
     serializerClass = fury.getClassResolver().getSerializerClass(clazz);
   }
 }
--- runtime/src/main/java/io/quarkus/fury/runtime/FurySerialization.java
+++ runtime/src/main/java/io/quarkus/fury/runtime/FurySerialization.java
@@ -13,4 +13,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface FurySerialization {
+  int classId() default -1;
 }
