952d603411d622cb72ab57ba12b0ba3f8f78f930
Jules Ivanic <jules.ivanic@gmail.com>
2026-08-21 13:45:17 +1000

    feat(json): add integration test and documentation for JSON support
    - Add integration-tests/json-rest module with GET/POST tests using
      application/json media type via ForyJsonSerializer
    - Register ForyJsonSerializer and ForyJsonProducer in ForyProcessor
      deployment step for both reactive and classic REST stacks
    - Add JSON Serialization section to docs/index.adoc showing usage
      alongside the existing binary serialization

--- deployment/src/main/java/io/quarkiverse/fory/deployment/ForyProcessor.java
+++ deployment/src/main/java/io/quarkiverse/fory/deployment/ForyProcessor.java
@@ -15,6 +15,8 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 import io.quarkiverse.fory.ForyBuildTimeConfig;
+import io.quarkiverse.fory.ForyJsonProducer;
+import io.quarkiverse.fory.ForyJsonSerializer;
 import io.quarkiverse.fory.ForyProducer;
 import io.quarkiverse.fory.ForyRecorder;
 import io.quarkiverse.fory.ForySerialization;
@@ -65,6 +67,7 @@ class ForyProcessor {
     @BuildStep
     void unremovableBeans(BuildProducer<AdditionalBeanBuildItem> beanProducer) {
         beanProducer.produce(AdditionalBeanBuildItem.unremovableOf(ForyProducer.class));
+        beanProducer.produce(AdditionalBeanBuildItem.unremovableOf(ForyJsonProducer.class));
     }
 
     @BuildStep
@@ -184,11 +187,15 @@ class ForyProcessor {
         if (capabilities.isPresent(Capability.RESTEASY)) {
             resteasyJaxrsProviderBuildItemBuildProducer
                     .produce(new ResteasyJaxrsProviderBuildItem(ForySerializer.class.getName()));
+            resteasyJaxrsProviderBuildItemBuildProducer
+                    .produce(new ResteasyJaxrsProviderBuildItem(ForyJsonSerializer.class.getName()));
         } else if (capabilities.isPresent(Capability.RESTEASY_REACTIVE)) {
             registerHandler(RuntimeType.SERVER, additionalReaders, additionalWriters);
+            registerJsonHandler(RuntimeType.SERVER, additionalReaders, additionalWriters);
         }
         if (capabilities.isPresent(Capability.REST_CLIENT_REACTIVE)) {
             registerHandler(RuntimeType.CLIENT, additionalReaders, additionalWriters);
+            registerJsonHandler(RuntimeType.CLIENT, additionalReaders, additionalWriters);
         }
     }
 
@@ -203,6 +210,21 @@ class ForyProcessor {
         additionalWriters.produce(new MessageBodyWriterBuildItem.Builder(
                 ReactiveForySerializer.class.getName(), Object.class.getName())
                 .setMediaTypeStrings(List.of("application/fory", "application/*+fory"))
+                .setRuntimeType(type)
+                .setBuiltin(true).build());
+    }
+
+    private void registerJsonHandler(RuntimeType type,
+            BuildProducer<MessageBodyReaderBuildItem> additionalReaders,
+            BuildProducer<MessageBodyWriterBuildItem> additionalWriters) {
+        additionalReaders.produce(new MessageBodyReaderBuildItem.Builder(
+                ForyJsonSerializer.class.getName(), Object.class.getName())
+                .setMediaTypeStrings(List.of("application/json"))
+                .setRuntimeType(type)
+                .setBuiltin(true).build());
+        additionalWriters.produce(new MessageBodyWriterBuildItem.Builder(
+                ForyJsonSerializer.class.getName(), Object.class.getName())
+                .setMediaTypeStrings(List.of("application/json"))
                 .setRuntimeType(type)
                 .setBuiltin(true).build());
     }
---
+++ integration-tests/json-rest/src/test/java/io/quarkiverse/fory/it/json/JsonRestForyIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.fory.it.json;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class JsonRestForyIT extends JsonRestForyTest {
+}
